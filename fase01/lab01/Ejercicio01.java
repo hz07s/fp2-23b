785b5f8b3e00e4227f35b79bf88173e8c6ba0329
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-17 10:39:27 -0500

    Agregando mensajes al recibir los datos de los soldados

--- fase01/lab01/Ejercicio01.java
+++ fase01/lab01/Ejercicio01.java
@@ -4,18 +4,24 @@
 
 import java.util.Scanner;
 class Ejercicio01 {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		String soldier1 = sc.next();
-		String soldier2 = sc.next();
-		String soldier3 = sc.next();
-		String soldier4 = sc.next();
-		String soldier5 = sc.next();
-		
-		System.out.println(soldier1);
-		System.out.println(soldier2);
-		System.out.println(soldier3);
-		System.out.println(soldier4);
-		System.out.println(soldier5);
-	}
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Ingrese el primer soldado: ");
+    String soldier1 = sc.next();
+    System.out.print("Ingrese el segundo soldado: ");
+    String soldier2 = sc.next();
+    System.out.print("Ingrese el tercer soldado: ");
+    String soldier3 = sc.next();
+    System.out.print("Ingrese el cuarto soldado: ");
+    String soldier4 = sc.next();
+    System.out.print("Ingrese el quinto soldado: ");
+    String soldier5 = sc.next();
+    
+    System.out.println("Lista de soldados: ");
+    System.out.println("Soldado Nro1 " + soldier1);
+    System.out.println("Soldado Nro2 " + soldier2);
+    System.out.println("Soldado Nro3 " + soldier3);
+    System.out.println("Soldado Nro4 " + soldier4);
+    System.out.println("Soldado Nro5 " + soldier5);
+  }
 }
