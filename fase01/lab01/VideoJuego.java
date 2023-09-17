39355c9124f834c6bdaa65ceb2b175c6a0e98ed8
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-17 09:04:01 -0500

    Agregando el saludo a VideoJuego.java con un mensaje de Bienvenida

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -2,6 +2,6 @@
 // Autor: Hernan Andy
 public class VideoJuego {
 	public static void main(String[] args){
-		System.out.println("Hello World");	
+		System.out.println("Bienvenido a mi Videojuego");	
 	}
 }
