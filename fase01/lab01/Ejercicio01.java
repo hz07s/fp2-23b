ad98e7ba894b88bc30b80b602d49d0f345a99faf
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-17 10:25:46 -0500

    Ejercicio01 realizado con variables simples (Strings) para los datos de 5 soldados

---
+++ fase01/lab01/Ejercicio01.java
@@ -0,0 +1,21 @@
+// Laboratorio Nro 01 - Ejercicio01
+// Autor : Hernan Andy
+// Colaboró : -
+
+import java.util.Scanner;
+class Ejercicio01 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String soldier1 = sc.next();
+		String soldier2 = sc.next();
+		String soldier3 = sc.next();
+		String soldier4 = sc.next();
+		String soldier5 = sc.next();
+		
+		System.out.println(soldier1);
+		System.out.println(soldier2);
+		System.out.println(soldier3);
+		System.out.println(soldier4);
+		System.out.println(soldier5);
+	}
+}
