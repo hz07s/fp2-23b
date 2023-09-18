d5b9cc2bd4ef8e6867bb1bc64b1b70b0874a57b1
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-17 20:46:43 -0500

    Corrigiendo el metodo armyWinner ya que no consideraba la posibilidad de un empate

--- fase01/lab01/Ejercicio05.java
+++ fase01/lab01/Ejercicio05.java
@@ -27,9 +27,11 @@ class Ejercicio05 {
 
   public static void armyWinner(String[] army1, String[] army2) {
     if (army1.length > army2.length) {
-      System.out.println("Ejercito Nro 1");
+      System.out.println(" El ganador es el Ejercito Nro 1");
+    } else if (army2.length > army1.length) {
+      System.out.println("El ganador es el Ejercito Nro 2");
     } else {
-      System.out.println("Ejercito Nro 2");
+      System.out.println("Es un empate entre los ejercitos");
     }
   }
 }
