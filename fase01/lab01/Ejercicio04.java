6759b1f9b19013e4ae98a9a0c54d57b11107f8e9
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-17 20:13:17 -0500

    Mostrando los nombres junto a la vida, utilizando el mismo bucle for

--- fase01/lab01/Ejercicio04.java
+++ fase01/lab01/Ejercicio04.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 class Ejercicio04 {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    
+
     String[] soldiers = new String[5];
     int[] healt = new int[5];
 
@@ -22,7 +22,7 @@ class Ejercicio04 {
     }
 
     for (int i = 0; i < soldiers.length; i++) {
-      System.out.println("Soldado Nro " + (i + 1) + " : " + soldiers[i]);
+      System.out.println("Soldado Nro " + (i + 1) + " : " + soldiers[i] + " - vida: " + healt[i]);
     }
 
   }
