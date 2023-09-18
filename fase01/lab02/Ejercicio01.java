c10359a65493c72d0548b951623998d02a93a9fb
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-17 22:31:08 -0500

    Segunda version del metodo con cambios para mostrar las letras adivinadas previamente al jugador

--- fase01/lab02/Ejercicio01.java
+++ fase01/lab02/Ejercicio01.java
@@ -125,15 +125,17 @@ public class Ejercicio01 {
 
   public static String mostrarBlancosActualizados(String letra, String palabra, String blancos){
     System.out.println("PROCESANDO...");
-
+    // la variable newBlancos sirve para guardar los valores de blancos y no perderlos
+    String newBlancos = "";
     char c = letra.charAt(0);
     for(int i = 0; i < palabra.length(); i++) {
       if (palabra.charAt(i) == c)
-        blancos += c + " ";
+        newBlancos += c + " ";
       else
-        blancos += blancos.charAt(i * 2) + " ";
+        newBlancos += blancos.charAt(i * 2) + " "; // asi usaremos el antiguo blancos
     }
     System.out.println(blancos);
-    return blancos;
+    // retornamos blancos y asi guardarlos para la proxima coincidencia de la palabraSecreta
+    return newBlancos;
   }
 }
