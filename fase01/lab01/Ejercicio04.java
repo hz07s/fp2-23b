c649f75890005085b7823643630431656e599553
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-17 20:18:09 -0500

    pequeña correcion en el numero de ejercicio al comienzo

--- fase01/lab01/Ejercicio04.java
+++ fase01/lab01/Ejercicio04.java
@@ -1,4 +1,4 @@
-// Laboratorio Nro 01 - Ejercicio03
+// Laboratorio Nro 01 - Ejercicio04
 // Autor : Hernan Andy
 // Colaboró : -
 // Tiempo : -
@@ -14,7 +14,7 @@ class Ejercicio04 {
     for (int i = 0; i < soldiers.length; i++) {
       System.out.print("Ingrese el nombre del soldado Nro " + (i + 1) + " : ");
       soldiers[i] = sc.next();
-      
+
       System.out.print("Vida: ");
       healt[i] = sc.nextInt();
     }
