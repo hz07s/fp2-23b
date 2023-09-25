e74655e4b2fea8b49c0e14606eb9ad200ee3d0cb
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-25 06:05:23 -0500

    Implementando el método ordenarPorPuntosInsercion haciendo uso del algoritmo proporcionado y adaptándolo al código

--- fase01/lab04/DemoBatalla.java
+++ fase01/lab04/DemoBatalla.java
@@ -198,9 +198,25 @@ public class DemoBatalla {
   }
   //Método que muestra las naves ordenadas por número de puntos de mayor a menor
   public static void ordenarPorPuntosInsercion(Nave[] flota){
+    for (int i = 1; i < flota.length; i++) {
+      int puntos = flota[i].getPuntos();
+      int j = i - 1;
+
+      while (j >= 0 && flota[j].getPuntos() > puntos) {
+          flota[j + 1] = flota[j];
+          j--;
+      }
+      flota[j + 1].setPuntos(puntos);
+    }
+
+    System.out.println("Flota ordenada por puntos (insercion): ");
+    for (int i = 0; i <  flota.length; i++){
+      System.out.println(flota[i].getNombre() + ": " + flota[i].getPuntos());
+    }
   }
   //Método que muestra las naves ordenadas por nombre de Z a A
   public static void ordenarPorNombreInsercion(Nave[] flota){
+
   }
   public static Nave[] crearArreglo(Nave[] flota){
     Nave[] flotaNueva = new Nave[flota.length];
