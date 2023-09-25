3d5d89559f82ea7e3214bd7aba8f121d1722f5d0
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-24 20:34:58 -0500

    Implementando el método para retornar un arreglo aleatoriamente desordenado

--- fase01/lab03/DemoBatalla.java
+++ fase01/lab03/DemoBatalla.java
@@ -91,5 +91,17 @@ public class DemoBatalla {
 
   //Crear un método que devuelva un nuevo arreglo de objetos con todos los objetos previamente ingresados 
   //pero aleatoriamente desordenados
-  
+
+  public static Nave [] desordenarFlota(Nave flota []){
+    Nave [] flotaRandom = new Nave [flota.length];
+    for (int i = 0; i < flota.length; i++) {
+      int r = (int)Math.random() * 10;
+      while (flotaRandom[r] != null){
+        r = (int)Math.random() * 10;
+      }
+      flotaRandom [r] = flota[i];
+    }
+
+    return flotaRandom;
+  }
 }
