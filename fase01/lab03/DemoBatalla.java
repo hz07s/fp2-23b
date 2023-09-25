fe54dba8ff0c39e51fbb70b0cdf83f7f128ca769
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-24 20:42:53 -0500

    Implementando el método mostrarNave, el cual está presente en otros métodos

--- fase01/lab03/DemoBatalla.java
+++ fase01/lab03/DemoBatalla.java
@@ -73,7 +73,13 @@ public class DemoBatalla {
     }
   }
   public static void mostrarNave(Nave nave){
-
+    System.out.println("\n Nave: " + nave.getNombre());
+    if (nave.getEstado())
+      System.out.println("Estado : Alive");
+      else
+      System.out.println("Estado : Dead");
+    System.out.println("Puntos : " + nave.getPuntos());
+    
   }
 
   //Método que devuelve la Nave con mayor número de Puntos
