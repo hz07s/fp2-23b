4cab8681ed075f7c20b609efefab8169e1a34686
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-09-24 20:17:01 -0500

    Implementando el método mostrarNaves, utilizando el futuro método mostrarNave

--- fase01/lab03/DemoBatalla.java
+++ fase01/lab03/DemoBatalla.java
@@ -41,6 +41,9 @@ public class DemoBatalla {
 
   //Método para mostrar todas las naves
   public static void mostrarNaves(Nave [] flota){
+    for (int i = 0; i < flota.length; i++){
+      mostrarNave(flota[i]);
+    }
   }
 
   //Método para mostrar todas las naves de un nombre que se pide por teclado
