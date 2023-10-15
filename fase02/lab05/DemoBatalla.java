298d6cf0f27fe67326cb98d8587455d26a106c0f
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-10-15 17:54:51 -0500

    Primer avance del metodo para graficar los soldados en una tabla

--- fase02/lab05/DemoBatalla.java
+++ fase02/lab05/DemoBatalla.java
@@ -2,19 +2,17 @@
 // Autor : Hernan Andy
 // Colaboro : -
 // Tiempo : -
-import java.util.*;
+//import java.util.*;
 public class DemoBatalla {
   static Soldado[][] army = new Soldado[10][10];
+  static Soldado[] amry1D = new Soldado[10];
   public static void main(String [] args){
   createArmy(army);
-  for (int i = 0; i < army.length; i++)
-    for (int k = 0; k < army[i].length; k++)
-      if (army[i][k] != null)
-        System.out.println(army[i][k]);
+  showArmy(army);
   }
 
   public static void createArmy(Soldado army[][]){
-    int numSoldiers = (int) (Math.random() * 10);
+    int numSoldiers = (int) (Math.random() * 10) + 1;
     
     for (int i = 0; i < numSoldiers; i++){
       int row, col;
@@ -25,12 +23,40 @@ public class DemoBatalla {
       } while (army[row][col] != null);
 
       Soldado s = new Soldado("Soldier" + i, row, (char) (col + 'A'), true, (int) (Math.random() * 5) + 1);
+      amry1D[i] = s;
       army[row][col] = s;
 
     }
   }
 
+  public static void showArmy(Soldado[][] army){
+    String linesUp = "  |       |       |       |       |       |       |       |       |       |";
 
-  
+    String linesDown = "  |_______|_______|_______|_______|_______|_______|_______|_______|_______|";
+
+    System.out.println("      A      B      C      D      E      F      G      H      I      J\n"
+                      +"   _______________________________________________________________________");
+    for (int r = 0; r < army.length; r++){
+      System.out.println(linesUp);
+      for (int c = 0; c < army[r].length; c++){
+        
+        
+      }
+      System.out.println(linesDown);
+    }
+
+    /*
+    Example:
+        A      B      C      D      E      F      G      H      I      J    
+     _______________________________________________________________________
+    |       |       |       |       |       |       |       |       |       |
+    |  S 1  |       |       |       |       |       |       |       |       |
+    |_______|_______|_______|_______|_______|_______|_______|_______|_______|
+
+
+
+
+     */
+  } 
 
 }
