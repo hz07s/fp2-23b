903d1c98abd3dedb3b72249a8f0d12acd297bfa2
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-12-10 08:59:59 -0500

    Agregando los atributos solicitados a la clase Soldado

--- fase02/lab12/Soldado.java
+++ fase02/lab12/Soldado.java
@@ -2,18 +2,23 @@ public class Soldado {
   private String name;
   private int row;
   private char column;
-  private boolean status;
-  private int health;
   private char team;
 
+  private int attackLevel;
+  private int attackDefense;
+  private int lifeLevel;
+  private int actualLife;
+  private int speed;
+  private String attitude;
+  private boolean lives;
+  
   public Soldado(String name, int row, char column, boolean status, int health, char team) {
     this.name = name;
     this.row = row;
     this.column = column;
-    this.status = status;
-    this.health = health;
     this.team = team;
   }
+
   //Setters
   public void setName(String n){
     name = n;
@@ -24,14 +29,30 @@ public class Soldado {
   public void setColumn(char c){
     column = c;
   }
-  public void setStatus(boolean s){
-    status = s;
-  }
-  public void setHealth(int h){
-    health = h;
-  }
   public void setTeam(char t){
     team = t;
+  }
+
+  public void setAttackLevel(int al){
+    attackLevel = al;
+  }
+  public void setAttackDefense(int ad){
+    attackDefense = ad;
+  }
+  public void setLifeLevel(int af){
+    lifeLevel = af;
+  }
+  public void setActualLife(int al){
+    actualLife = al;
+  }
+  public void setSpeed(int s){
+    speed = s;
+  }
+  public void setAttitude(String a){
+    attitude = a;
+  }
+  public void setLives(boolean l){
+    lives = l;
   }
 
   //Getters
@@ -44,23 +65,47 @@ public class Soldado {
   public char getColumn(){
     return column;
   }
-  public boolean getStatus(){
-    return status;
-  }
-  public int getHealth(){
-    return health;
-  }
   public char getTeam(){
     return team;
   }
+
+  public int getAttackLevel(){
+    return attackLevel;
+  }
+  public int getAttackDefense(){
+    return attackDefense;
+  }
+  public int getLifeLevel(){
+    return lifeLevel;
+  }
+  public int getActualLife(){
+    return actualLife;
+  }
+  public int getSpeed(){
+    return speed;
+  }
+  public String getAttitude(){
+    return attitude;
+  }
+  public boolean getLives(){
+    return lives;
+  }
+  
   @Override
   public String toString() {  
     return "Data { " + 
-              "\n Name:   " + name   + 
-              "\n Row:    " + row    + 
-              "\n Column: " + column + 
-              "\n Status: " + status + 
-              "\n Health: " + health + 
+              "\n Name:   "        + name          + 
+              "\n Row:    "        + row           + 
+              "\n Column: "        + column        + 
+              "\n Team: "          + team          + 
+              "\n AttackLevel: "   + attackLevel   + 
+              "\n AttackDefense: " + attackDefense + 
+              "\n LifeLevel: "     + lifeLevel     + 
+              "\n ActualLife: "    + actualLife    + 
+              "\n Speed: "         + speed         + 
+              "\n Attitude: "      + attitude      + 
+              "\n Lives: "         + lives         + 
               "\n}\n";
+              
   }
 }
