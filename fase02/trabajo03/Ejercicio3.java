0dc1ad03344f03e98adf9cf93f6a990353f88aec
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-12-23 00:46:19 -0500

    Comenzando con la base de las interfaces y clases

---
+++ fase02/trabajo03/Ejercicio3.java
@@ -0,0 +1,19 @@
+public class Ejercicio3 {
+
+  public static void main(String[] args) {
+    Hidroavion hidroavion = new Hidroavion();
+    
+  }
+}
+
+interface Barco {
+
+}
+
+interface Avion {
+
+}
+
+class Hidroavion implements Barco, Avion {
+  
+}
