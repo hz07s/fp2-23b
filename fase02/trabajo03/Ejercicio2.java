fb9c7a4f6f1f8486f3e03bad517c2a3bdf4e91f3
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-12-23 00:35:11 -0500

    Implementando getters y setters a la clase Cilindro.java y realizando una prueba

--- fase02/trabajo03/Ejercicio2.java
+++ fase02/trabajo03/Ejercicio2.java
@@ -1,8 +1,26 @@
 public class Ejercicio2 {
-  
+  public static void main(String[] args) {
+    Cilindro cilindro = new Cilindro();
+
+    cilindro.setCentro(3.0);
+    cilindro.setRadio(2.5);
+    cilindro.setLongitud(4.0);
+
+    double superficieCilindro = cilindro.superficie();
+    System.out.println("La superficie del cilindro es: " + superficieCilindro);
+  }
 }
 class Cilindro extends Circulo{
   private double longitud;
+
+  public void setLongitud(double longitud) {
+    this.longitud = longitud;
+  }
+
+  public double getLongitud() {
+    return longitud;
+  }
+
   public double superficie() {
     return 2 * radio * (longitud + radio);
   }
