636b3422c6233f6cdfa89b0658dbd1d92aff2f6c
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-12-23 00:08:05 -0500

    Creando las primeras clases con los atributos solicitados

---
+++ fase02/trabajo03/Ejercicio1.java
@@ -0,0 +1,33 @@
+class Ejercicio1 {
+
+  
+}
+
+class Punto {
+  private double x;
+  private double y;
+  
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public double getX(){
+    return x;
+  }
+  
+  public double getY(){
+    return y;
+  }
+
+  public double distancia(Punto p) {
+    double x2 = p.getX();
+    double y2 = p.getY();
+    
+    return ;
+  }
+}
+
+class Circulo extends Punto{
+  private double centro;
+  private double radio;
+}
