725fb53afcf2caa23b83559b892265f42c692bb8
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2023-12-23 00:23:00 -0500

    Se concluyo el ejercicio y realizando una prueba de su funcionamiento

--- fase02/trabajo03/Ejercicio1.java
+++ fase02/trabajo03/Ejercicio1.java
@@ -1,6 +1,17 @@
 class Ejercicio1 {
+  public static void main(String[] args){
+    Punto p1 = new Punto();
+    Punto p2 = new Punto();
+    
+    p1.setX(1.0);
+    p1.setY(4.5);
 
-  
+    p2.setX(2.0);
+    p2.setY(6.0);
+
+    double distancia = p1.distancia(p2);
+    System.out.println("La distancia entre el punto 1 y el punto 2 es: " + distancia);    
+  }
 }
 
 class Punto {
@@ -15,12 +26,16 @@ class Punto {
     return x;
   }
   
+  public void setY(double y) {
+    this.y = y;
+  }
+
   public double getY(){
     return y;
   }
 
   public double distancia(Punto p) {
-    return Math.pow((p.getX() - x),2) + Math.pow(Math.pow((p.getY() - y),2),1/2);
+    return Math.sqrt(Math.pow((p.getX() - x), 2) + Math.pow((p.getY() - y), 2));
   }
 }
 
