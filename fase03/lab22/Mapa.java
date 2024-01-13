1a0586ff7f8bb3b99f7e0c7a74e9bb209487b2c5
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-01-13 12:33:05 -0500

    Terminada la clase mapa

--- fase03/lab22/Mapa.java
+++ fase03/lab22/Mapa.java
@@ -1,21 +1,34 @@
 public class Mapa {
-  private String territory;
+  private static Mapa instanciaSingleton;
   private String[] typesTerritory ={"bosque","campoAbierto", "montaña", "desierto", "playa"};
-  public Soldado[][] board;
-  public String randomMapa (){
-    this.territory =  typesTerritory[(int) (Math.random() * typesTerritory.length)];
-    return territory;
-  }
+  private String territory;
+  private Soldado[][] board;
   
+  private Mapa(){}
 
-
-
+  public static Mapa getInstance() {
+    if (instanciaSingleton == null) 
+      instanciaSingleton = new Mapa();
+    return instanciaSingleton;
+  }
   
   public void setTerritory(String territory) {
     this.territory = territory;
   }
-
+  
   public String getTerritory() {
     return territory;
+  }
+
+  public void setBoard(Soldado[][] board){
+    this.board = board;
+  }
+
+  public Soldado[][] getBoard(){
+    return board;
+  }
+
+  public void setRandomMapa (){
+    this.territory =  typesTerritory[(int) (Math.random() * typesTerritory.length)];
   }
 }
