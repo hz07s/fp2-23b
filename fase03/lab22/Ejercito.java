94bd50accf6a9c7aaf3aebd286fbfba93da2e5d8
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-01-13 16:37:17 -0500

    Redefiniendo la clase Ejercito, adaptando y reorganizando su contenido

--- fase03/lab22/Ejercito.java
+++ fase03/lab22/Ejercito.java
@@ -5,13 +5,20 @@ public class Ejercito {
   private ArrayList<Soldado> army;
   
   public Ejercito(Mapa map){
-    String[] reinos = {"Castilla", "Aragon", "Moros", "Sacro Imperio Romano Germanico"};
+    String[] reinos = {"Inglaterra", "Francia","Castilla-Aragon",
+                       "Moros", "Sacro Imperio Romano-Germanico"};
     kingdom = reinos[(int) (Math.random() * reinos.length)];
     
     int n = (int) (Math.random() * 10) + 1;
     for (int i = 0; i < n; i++) {
       Soldado s = new Soldado(map, this, name);
       army.add(s);
+    }
+    if (benefits(map)){
+      for (Soldado s : army){
+        s.setActualLife(s.getActualLife() + 1);
+        s.setLifeLevel(s.getLifeLevel() + 1);
+      }
     }
   }
 
@@ -33,5 +40,33 @@ public class Ejercito {
 
   public char getName(){
     return name;
+  }
+
+  public boolean benefits(Mapa m){
+    String t = m.getTerritory();
+    switch (kingdom) {
+      case ("Inglaterra") -> {
+        if (t.equals("Bosque"))
+          return true;
+      }
+      case ("Francia") -> {
+        if (t.equals("Campo Abierto"))
+          return true;
+      }
+      case ("Castilla-Aragon") -> {
+        if (t.equals("Montaña"))
+          return true;
+      }
+      case ("Moros") -> {
+        if (t.equals("Desierto"))
+          return true;
+      }
+      case ("Sacro Imperio Romano-Germanico") -> {
+        if (t.equals("Bosque") || t.equals("Playa") || 
+            t.equals("Campo Abierto"))
+          return true;
+      }
+    }
+    return false;
   }
 }
