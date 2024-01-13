1ef90dff157ceb7ca5f3034ee799dd048c7fd814
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-01-13 13:14:00 -0500

    Agregando a la clase Ejercito metodos y atributos que se usaran mas adelante

--- fase03/lab22/Ejercito.java
+++ fase03/lab22/Ejercito.java
@@ -1,7 +1,23 @@
 import java.util.ArrayList;
 public class Ejercito {
   private String kingdom;
+  private char name;
   private ArrayList<Soldado> army;
+  
+  public Ejercito(Mapa map){
+    String[] reinos = {"Castilla", "Aragon", "Moros", "Sacro Imperio Romano Germanico"};
+    kingdom = reinos[(int) (Math.random() * reinos.length)];
+    
+    int n = (int) (Math.random() * 10) + 1;
+    for (int i = 0; i < n; i++) {
+      Soldado s = new Soldado(map, this, name);
+      army.add(s);
+    }
+  }
+
+  public Ejercito(int n){
+    //Ejercito personalizado
+  }
 
   public void setKingdom(String kingdom){
     this.kingdom = kingdom;
@@ -11,9 +27,11 @@ public class Ejercito {
     return kingdom;
   }
 
+  public void setName(char name){
+    this.name = name;
+  }
 
-
-  public void createArmy(Mapa map) {
-    
+  public char getName(){
+    return name;
   }
 }
